c6031abe41c6f68b0e35d54dc33f9831aae5e9b6
Lucas Gabriel <lg139826065@gmail.com>
2023-12-01 23:23:22 -0300

    Atualizacão

---
+++ JS/web.js
@@ -0,0 +1,86 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+   menu.classList.toggle('ativo');
+   NavMenu.classList.toggle('ativo');
+})
+//Menu Responsivo
+
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal().reveal('.area-1', {
+   origin: 'top',
+   distance: '30px',
+   duration: 1000,
+
+});
+
+
+
+ScrollReveal().reveal('.text-page-h2', {
+   origin: 'top',
+   distance: '30px',
+   duration: 700,
+});
+
+ScrollReveal().reveal('.home-text', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1350,
+});
+
+ScrollReveal().reveal('.text-h4', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1000,
+});
+
+ScrollReveal().reveal('.text-p', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1350,
+});
+
+ScrollReveal().reveal('.h2-sobre', {
+   origin: 'top',
+   distance: '30px',
+   duration: 800,
+});
+
+ScrollReveal().reveal('.p-sobre_1', {
+   origin: 'left',
+   distance: '30px',
+   duration: 800,
+});
+
+ScrollReveal().reveal('.p-sobre_2', {
+   origin: 'right',
+   distance: '30px',
+   duration: 900,
+});
+
+ScrollReveal().reveal('#footer_social_media', {
+   origin: 'top',
+   distance: '30px',
+   duration: 800,
+})
+
+ScrollReveal().reveal('.footer-list', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1000,
+})
+
+ScrollReavel().reavel('.input_group', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1000,
+})
+
+ScrollReavel().reavel('.titulo-card', {
+   origin: 'left',
+   distance: '30px',
+   duration: 1000,
+})
+
+//2 Animação com Scroll reavel 
---
+++ script.js
@@ -0,0 +1,11 @@
+function comprarNovamente() {
+    alert('Ação: Comprar Novamente');
+}
+
+function voltar() {
+    alert('Ação: Voltar');
+}
+
+function sair() {
+    alert('Ação: Sair');
+}
